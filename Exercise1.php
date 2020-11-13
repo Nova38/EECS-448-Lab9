059c3ae0f4d4ba0b7660351ad587641f48653143
Thomas Atkins <thomas.atkins@ku.edu>
2020-11-13 12:24:16 -0600

    Exercise1 done

---
+++ Exercise1.php
@@ -0,0 +1,61 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+//Inside myfirstprogram.php
+function tb($product) {
+ $z = "<td>". $product ."</td>";
+ return $z;
+}
+function product($x, $y){
+    $product = $x * $y;
+    return $product;
+}
+
+echo "<table>";
+
+echo "<tr>";
+for ($i = 0; $i <= 100; $i++) {
+    if($i == 0 ){
+        echo "<th> </th>";
+    }else{
+
+        echo "<th>". product(1,$i) . "</th>";
+    }
+}
+echo "</tr>";
+
+
+for ($i = 1; $i <= 100; $i++) {
+    echo "<tr>";
+    for ($j = 0; $j <= 100; $j++) {
+        if($j == 0 ){
+            echo "<th>". $i. "</th>";
+        }else{
+
+            echo "<th>". product($i,$j) . "</th>";
+        }
+    }
+    echo "</tr>";
+
+}
+
+// for ($i = 0; $i <= 100; $i++) {
+//     echo "<tr>";
+//     if($i = 0){
+//         for ($j = 0; $j <= 100; $j++) {
+//             echo tb($j,1);
+
+//         }
+//     }
+//     else{
+//         for ($j = 0; $j <= 100; $j++) {
+//             echo tb($j,$i);
+
+//         }
+//     }
+
+//     echo "</tr>";
+// }
+
+echo "</table>";
